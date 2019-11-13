fb050b6af84c85ffa76bf18f424fe9c50559ef2f
SophieTolk <sophie.tolk@capgemini.com>
2019-11-13 11:18:45 +0100

    dgdg

--- src/helloworld/Main.java
+++ src/helloworld/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.println(" heldfsaa");
+        System.out.println("Hello world");
     }
 }
